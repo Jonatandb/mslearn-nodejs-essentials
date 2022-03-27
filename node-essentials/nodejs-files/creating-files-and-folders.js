f13ed1489de2976ca1ea0fe62c92ddb7c2f4a932
Jonatandb <jonatandb@gmail.com>
2022-03-27 17:54:12 -0300

    Exercise: Creating files and folders - Solved

---
+++ node-essentials/nodejs-files/creating-files-and-folders.js
@@ -0,0 +1,43 @@
+const fs = require('fs').promises
+const path = require('path')
+
+async function findSalesFiles(folderName) {
+  let salesFiles = []
+
+  async function findFiles(folderName) {
+    const items = await fs.readdir(folderName, { withFileTypes: true })
+
+    for (item of items) {
+      if (item.isDirectory()) {
+        await findFiles(path.join(folderName, item.name))
+      } else {
+        if (path.extname(item.name) === '.json') {
+          salesFiles.push(path.join(folderName, item.name))
+        }
+      }
+    }
+  }
+
+  await findFiles(folderName)
+
+  return salesFiles
+}
+
+async function main() {
+  const salesDir = path.join(__dirname, 'stores')
+  const salesTotalsDir = path.join(__dirname, 'salesTotals')
+
+  try {
+    await fs.mkdir(salesTotalsDir)
+  } catch (e) {
+    console.log(`${salesTotalsDir} already exists.`)
+  }
+
+  const salesFiles = await findSalesFiles(salesDir)
+  console.log(salesFiles)
+
+  await fs.writeFile(path.join(salesTotalsDir, 'totals.txt'), String())
+  console.log(`Wrote sales totals to ${salesTotalsDir}`)
+}
+
+main()
